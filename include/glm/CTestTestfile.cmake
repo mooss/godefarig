167658719e88efa6d620fe055b79fb3d167034c0
mooss <mooss@protonmail.com>
2017-03-29 02:56:35 +0200

    removed glm source

--- include/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mooss/godefarig/glm-0.9.7.5/glm
-# Build directory: /home/mooss/godefarig/glm-0.9.7.5/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

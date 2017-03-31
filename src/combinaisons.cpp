61e449fef0c0d08f6ae8bcf66342d05feda7b80d
mooss <mooss@protonmail.com>
2017-03-31 08:36:15 +0200

    created input subfolder

--- src/combinaisons.cpp
+++
@@ -1,9 +0,0 @@
-#include "combinaisons.h"
-
-unsigned long factorial(unsigned int nbr)
-{
-    unsigned long result;
-    for(result = 1; nbr > 1; --nbr)
-        result *= nbr;
-    return result;
-}

0f13ea547da99eba3284cd44309e0457afb4517a
mooss <mooss@protonmail.com>
2017-10-15 17:11:45 +0200

    comparing hex cardinal obtained via hexagon_iteration and hex cardinal obtained via hexagon::number_at_stage

--- src/terrain_test.cpp
+++ src/terrain_test.cpp
@@ -22,26 +22,33 @@
 
 int main(int argc, char** argv)
 {
-    gfg::fractal_octahedron octahedron(3);
+    unsigned int stage = 8;
+    gfg::fractal_octahedron octahedron(stage);
 
     std::cout << "created an octahedron of rank " << octahedron.rank()
               << std::endl;
+    // for(gfg::hexagonal_iterator hexit(octahedron); hexit != gfg::hexagonal_iterator::end(); ++hexit)
+    // {
+    //     std::cout << "\t" << hexit.index() << "\n";
+    // }
 
-    gfg::hexagonal_iterator hexiter(octahedron, 2);
-    unsigned int hex_card = 0;
-//    for(std::size_t i = 0; i < 21; ++i)
-    for(; hexiter != gfg::hexagonal_iterator::end(); ++hexiter)
+    for(unsigned int current_stage = 0; current_stage <= stage; ++current_stage)
     {
-        ++hex_card;
-        std::cout << "hex: " << hexiter.index() << std::endl;
-        std::cout << "\tneighbour 0 : " << hexiter->vertex(0) << std::endl;
-        std::cout << "\tneighbour 1 : " << hexiter->vertex(1) << std::endl;
-        std::cout << "\tneighbour 2 : " << hexiter->vertex(2) << std::endl;
-        std::cout << "\tneighbour 3 : " << hexiter->vertex(3) << std::endl;
-        std::cout << "\tneighbour 4 : " << hexiter->vertex(4) << std::endl;
-        std::cout << "\tneighbour 5 : " << hexiter->vertex(5) << std::endl;
+        unsigned int hex_card = 0;
+        for(gfg::hexagonal_iterator hexiter(octahedron, current_stage);
+            hexiter != gfg::hexagonal_iterator::end();
+            ++hexiter)
+        {
+            ++hex_card;
+        }
+        std::cout << "stage " << current_stage << " :\n\t"
+                  << hex_card  << "/" << gfg::hexagon::number_at_stage(current_stage) << " hex ; "
+                  << gfg::point::number_at_stage(current_stage) << " points ; "
+                  << gfg::face::number_at_stage(current_stage) << " faces ; "
+                  << gfg::edge::number_at_stage(current_stage) << " edges" << std::endl;
+
     }
-    std::cout << hex_card  << " hexagon iterated through" << std::endl;
+//    for(std::size_t i = 0; i < 21; ++i)
     
     return 0;
 }
